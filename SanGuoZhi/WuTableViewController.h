48266b87106a33b6bc6634f20b400669046895d2
YuJianSirius <YuJianSirius@aol.com>
2014-05-23 22:18:48 +0800

    add wu

---
+++ SanGuoZhi/WuTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  WuTableViewController.h
+//  SanGuoZhi
+//
+//  Created by xiaojia on 5/23/14.
+//  Copyright (c) 2014 sirius. All rights reserved.
+//
+
+#import "WeiTableViewController.h"
+
+@interface WuTableViewController : WeiTableViewController
+
+@end
